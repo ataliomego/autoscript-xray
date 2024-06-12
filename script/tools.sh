9de021bfc401f02965c8e8440414ec13350f10a6
Kyelva <niraid.mgp@gmail.com>
2024-06-12 21:19:46 +0800

    Update tools.sh

--- script/tools.sh
+++ script/tools.sh
@@ -1,1 +1,88 @@
+#!/bin/bash
+#Sceipt by AtaLioMego™  || Acha Wilbertha
+
+clear
+red='\e[1;31m'
+green='\e[1;32m'
+yell='\e[1;33m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+if [[ -e /etc/debian_version ]]; then
+	source /etc/os-release
+	OS=$ID # debian or ubuntu
+elif [[ -e /etc/centos-release ]]; then
+	source /etc/os-release
+	OS=centos
+fi
+
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "memeriksa vps anda"
+sleep 0.5
+CEKEXPIRED () {
+        today=$(date -d +1day +%Y -%m -%d)
+        Exp1=$(curl -sS https://raw.githubusercontent.com/yardproject/hele/main/ip | grep $MYIP | awk '{print $3}')
+        if [[ $today < $Exp1 ]]; then
+        echo "status script aktif.."
+        else
+        echo "SCRIPT ANDA EXPIRED";
+        exit 0
+fi
+}
+IZIN=$(curl -sS https://raw.githubusercontent.com/yardproject/hele/main/ip | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo "Keygen Terdeteksi: 1171114160-88numb-Ataliomego"
+echo "Terimakasih Telah memilih layanan Server Tunnel VPN kami! "
+
+else
+echo "Akses di tolak!! Po'u Nia Dulu Broooo!!";
+echo "Hubungi: wa.me/6281333398151";
+exit 0
+fi
+
+echo "Tools install...!"
+echo "Progress..."
+sleep 2
+
+apt update -y
+apt update -y
+apt dist-upgrade -y
+apt-get remove --purge ufw firewalld -y 
+apt-get remove --purge exim4 -y 
+apt-get remove apache2 -y 
+
+apt install -y screen curl jq bzip2 gzip coreutils rsyslog iftop \
+ htop zip unzip net-tools sed gnupg gnupg1 \
+ bc sudo apt-transport-https build-essential dirmngr libxml-parser-perl neofetch screenfetch git lsof \
+ openssl fail2ban tmux \
+ stunnel4 vnstat squid3 \
+ dropbear  libsqlite3-dev \
+ socat cron bash-completion ntpdate xz-utils sudo apt-transport-https \
+ gnupg2 dnsutils lsb-release chrony
+
+curl -sSL https://deb.nodesource.com/setup_16.x | bash - 
+sudo apt-get install nodejs -y
+
+/etc/init.d/vnstat restart
+wget -q https://humdi.net/vnstat/vnstat-2.6.tar.gz
+tar zxvf vnstat-2.6.tar.gz
+cd vnstat-2.6
+./configure --prefix=/usr --sysconfdir=/etc >/dev/null 2>&1 && make >/dev/null 2>&1 && make install >/dev/null 2>&1
+cd
+vnstat -u -i $NET
+sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
+chown vnstat:vnstat /var/lib/vnstat -R
+systemctl enable vnstat
+/etc/init.d/vnstat restart
+rm -f /root/vnstat-2.6.tar.gz >/dev/null 2>&1
+rm -rf /root/vnstat-2.6 >/dev/null 2>&1
+fi
+
+apt install -y libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd
+
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+yellow "Dependencies successfully installed..."
+sleep 3
+clear
 
